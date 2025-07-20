b8579337857c1bcb95438d803a9a25b5c38580f1
Zunaira Asad <zunairaasad10@gmail.com>
2025-07-20 12:31:33 -0700

    cart icon changed from header

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="shop.php">Shop</a></li>
         <li><a href="about.php">About</a></li>
-        <li><a href="cart.php"><i class="fas fa-shopping-cart"></i></a></li>
+        <li><a href="cart.php"> Cart</a></li>
         <li><a href="contact.php">Contact</a></li>
         <li><a href="login.php">Login</a></li>
     </ul>
