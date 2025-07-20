7180be2679e5e15b58fdd183eb735eae9ec847e5
Zunaira Asad <zunairaasad10@gmail.com>
2025-07-20 12:26:59 -0700

    changes header

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,10 @@
 <nav class="navbar">
-    <div class="logo">MyShop</div>
+    <div class="logo"><h1><b>Canvas and Craft</b></h1></div>
     <ul class="nav-links">
         <li><a href="index.php">Home</a></li>
         <li><a href="shop.php">Shop</a></li>
         <li><a href="about.php">About</a></li>
+        <li><a href="cart.php"><i class="fas fa-shopping-cart"></i></a></li>
         <li><a href="contact.php">Contact</a></li>
         <li><a href="login.php">Login</a></li>
     </ul>
