414ac1cf0d16da70c19dde950666a02dc7a6735b
Zunaira Asad <zunairaasad10@gmail.com>
2025-07-20 11:59:04 -0700

    My local changes before pull

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,9 @@
 <nav class="navbar">
-    <div class="logo"><strong><i class="fas fa-paint-brush"></i> Canvas & Craft</strong></div>
+    <div class="logo">MyShop</div>
     <ul class="nav-links">
         <li><a href="index.php">Home</a></li>
         <li><a href="shop.php">Shop</a></li>
         <li><a href="about.php">About</a></li>
-        <li><a href="cart.php"><i class="fas fa-shopping-cart"></i></a></li>
         <li><a href="contact.php">Contact</a></li>
         <li><a href="login.php">Login</a></li>
     </ul>
